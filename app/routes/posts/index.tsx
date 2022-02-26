fc212d118184419dd4e78baed05ab1bb7a96aa24
sergnio <sergnioa@gmail.com>
2022-02-26 13:19:31 -0600

    add type checking to fs

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -5,6 +5,7 @@ export const loader = async (): Promise<Post[]> => getPosts();
 
 export default function Posts() {
   const posts = useLoaderData<Post[]>();
+  console.log({ posts });
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
