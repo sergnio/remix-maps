ebd322ded77a840e7ffa351801649c52f927f483
sergnio <sergnioa@gmail.com>
2022-02-26 13:11:14 -0600

    add all files

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,22 @@
+import { Link, useLoaderData } from "remix";
+import getPosts, { Post } from "~/api/getPosts";
+
+export const loader = async (): Promise<Post[]> => getPosts();
+
+export default function Posts() {
+  const posts = useLoaderData<Post[]>();
+
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Posts page</h1>
+      <Link to="/">Routes</Link>
+      <ul>
+        {posts.map(({ slug, title }) => (
+          <li key={slug}>
+            <Link to={slug}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
